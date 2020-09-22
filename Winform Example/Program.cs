74f8b6af8f83c05f52e73f0b17c238b8ca70ade6
Absolute Zero <56744265+Absolute-0-Zero@users.noreply.github.com>
2020-09-22 13:27:06 +0100

    Added Winform Example

---
+++ Winform Example/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+using Winform_Example.Classes;
+
+namespace Winform_Example
+{
+    static class Program
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Detect if your application is running in a Virual Machine / Sandboxie...
+            // Anti_Analysis.Init();
+            //This connects your file to the AuthGuard.net API, and sends back your application settings and such
+            Guard.Initialize("PROGRAMSECRET", "VERSION", "VARIABLESECRET");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Register());
+        }
+    }
+}
